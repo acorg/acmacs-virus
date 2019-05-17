90742aeac3a8ec2d0ac6abc0ee60d883fa535d4a
Eugene Skepner <github@skepner.eu>
2019-05-17 18:30:42 +0200

    acmacs::virus_name::parse_name

--- cc/virus-name.cc
+++ cc/virus-name.cc
@@ -8,43 +8,58 @@
 
 // ----------------------------------------------------------------------
 
+#define SRE_AB "([AB])"
 #define SRE_HOST "(?:\\s*([A-Z \\-_]+)\\s*/)?"
-#define SRE_LOC "([A-Z \\-\\.,'&_]{2,})"  // no digits!, otherwise possibility to omit / after LOC does not work
-#define SRE_ISOLATION "\\s*0*([^/]+)\\s*" // mutiple / at the end (found in gisaid)
+// #define SRE_LOC "([A-Z \\-\\.,'&_]{2,})"  // no digits!, otherwise possibility to omit / after LOC does not work
+#define SRE_LOC "([A-Z0-9 \\-\\.,'&_]{2,})"
+#define SRE_LOC_NO_DIGITS "([A-Z \\-\\.,'&_]{2,})"
+#define SRE_ISOLATION "\\s*0*([^/]+)\\s*"
+#define SRE_ISOLATION_WITH_LOC "\\s*([A-Z]{3,})([^/]+)\\s*"
 
 constexpr const char* sre_flu_name_general_AB =
-        "\\b([AB])/"                    // type \1
+        "\\b"
+        SRE_AB "/"                      // type \1
         SRE_HOST                        // host \2
         SRE_LOC "/"                     // location \3
-        SRE_ISOLATION  "/+"             // isolation \4 - without leading 0, mutiple / at the end (found in gisaid)
+        SRE_ISOLATION  "/+"             // isolation \4  - without leading 0, mutiple / at the end (found in gisaid)
         "\\s*(\\d+)"                    // year \5 - any number of digits
         ;
 
-constexpr const char* sre_flu_name_general_AB_no_slash_after_location =
-        "\\b([AB])/"                    // type \1
+constexpr const char* sre_flu_name_general_AB_isolation_with_location =
+        "\\b"
+        SRE_AB "/"                      // type \1
         SRE_HOST                        // host \2
-        SRE_LOC                         // location \3, no slash!
-        SRE_ISOLATION  "/+"             // isolation \4 - without leading 0, mutiple / at the end (found in gisaid)
+        SRE_LOC "/"                     // location \3
+        SRE_ISOLATION_WITH_LOC  "/"     // isolation \4 + \5
         "\\s*(\\d+)"                    // year \5 - any number of digits
         ;
 
-constexpr const char* sre_flu_name_general_AB_numeric_isolation_split = // A/Zambia/13/177/2013
-        "\\b([AB])/"                    // type \1
+constexpr const char* sre_flu_name_general_AB_no_isolation = // or no slash after location
+        "\\b"
+        SRE_AB "/"                      // type \1
         SRE_HOST                        // host \2
-        SRE_LOC "/"                     // location \3
-        "\\s*0*(\\d+)\\s*/"             // first isolation \4, ignore leading 0
-        "\\s*(\\d+)\\s*/"               // second isolation \5
-        "\\s*(\\d+)"                    // year \6 - any number of digits
+        SRE_LOC_NO_DIGITS               // location \3
+        SRE_ISOLATION "/"               // isolation \4
+        "\\s*(\\d+)"                    // year \5 - any number of digits
         ;
 
-constexpr const char* sre_flu_name_general_AB_location_split = // A/Lyon/CHU/R18.50.47/2018
-        "\\b([AB])/"                    // type \1
-        SRE_HOST                        // host \2
-        SRE_LOC "/"                     // location \3
-        SRE_LOC "/"                     // second location \4
-        SRE_ISOLATION  "/+"             // isolation \5 - without leading 0, mutiple / at the end (found in gisaid)
-        "\\s*(\\d+)"                    // year \6 - any number of digits
-        ;
+// constexpr const char* sre_flu_name_general_AB_numeric_isolation_split = // A/Zambia/13/177/2013
+//         "\\b([AB])/"                    // type \1
+//         SRE_HOST                        // host \2
+//         SRE_LOC "/"                     // location \3
+//         "\\s*0*(\\d+)\\s*/"             // first isolation \4, ignore leading 0
+//         "\\s*(\\d+)\\s*/"               // second isolation \5
+//         "\\s*(\\d+)"                    // year \6 - any number of digits
+//         ;
+
+// constexpr const char* sre_flu_name_general_AB_location_split = // A/Lyon/CHU/R18.50.47/2018
+//         "\\b([AB])/"                    // type \1
+//         SRE_HOST                        // host \2
+//         SRE_LOC "/"                     // location \3
+//         SRE_LOC "/"                     // second location \4
+//         SRE_ISOLATION  "/+"             // isolation \5 - without leading 0, mutiple / at the end (found in gisaid)
+//         "\\s*(\\d+)"                    // year \6 - any number of digits
+//         ;
 
 constexpr const char* sre_flu_name_general_A_subtype =
         "\\b(A\\(H[1-9][0-9]?(?:N[1-9][0-9]?)?\\))/" // A(H3N2) \1
@@ -59,7 +74,7 @@ constexpr const char* sre_extra_keywords = "\\b(?:NEW)\\b";
 constexpr const char* sre_extra_keywords_when_reassortant = "\\b(?:HY)\\b";
 constexpr const char* sre_extra_symbols = "^[\\(\\)_\\s]+$";
 
-static std::string fix_location(std::string source, acmacs::virus::parse_name_f flags);
+static std::string fix_location(std::string source, acmacs::virus::parse_name_f flags, bool raise = true);
 static std::string fix_year(std::string source);
 
 // ----------------------------------------------------------------------
@@ -68,9 +83,10 @@ std::tuple<acmacs::virus::virus_name_t, acmacs::virus::Reassortant, acmacs::viru
 {
 #include "acmacs-base/global-constructors-push.hh"
     static const std::regex re_flu_name_general_AB{sre_flu_name_general_AB};
-    static const std::regex re_flu_name_general_AB_no_slash_after_location{sre_flu_name_general_AB_no_slash_after_location};
-    static const std::regex re_flu_name_general_AB_numeric_isolation_split{sre_flu_name_general_AB_numeric_isolation_split};
-    static const std::regex re_flu_name_general_AB_location_split{sre_flu_name_general_AB_location_split};
+    static const std::regex re_flu_name_general_AB_isolation_with_location{sre_flu_name_general_AB_isolation_with_location};
+    static const std::regex re_flu_name_general_AB_no_isolation{sre_flu_name_general_AB_no_isolation};
+    // static const std::regex re_flu_name_general_AB_numeric_isolation_split{sre_flu_name_general_AB_numeric_isolation_split};
+    // static const std::regex re_flu_name_general_AB_location_split{sre_flu_name_general_AB_location_split};
     static const std::regex re_flu_name_general_A_subtype{sre_flu_name_general_A_subtype};
     static const std::regex re_extra_keywords{sre_extra_keywords};
     static const std::regex re_extra_keywords_when_reassortant{sre_extra_keywords_when_reassortant};
@@ -83,28 +99,63 @@ std::tuple<acmacs::virus::virus_name_t, acmacs::virus::Reassortant, acmacs::viru
     std::string extra;
 
     const std::string source_u = ::string::upper(source);
-    if (std::smatch match_general_AB_numeric_isolation_split; std::regex_search(source_u, match_general_AB_numeric_isolation_split, re_flu_name_general_AB_numeric_isolation_split)) {
-        const std::array fields{match_general_AB_numeric_isolation_split[1].str(), match_general_AB_numeric_isolation_split[2].str(),
-                                fix_location(match_general_AB_numeric_isolation_split[3].str(), flags & parse_name_f::lookup_location),
-                                ::string::concat(match_general_AB_numeric_isolation_split[4].str(), '-', match_general_AB_numeric_isolation_split[5].str()),
-                                fix_year(match_general_AB_numeric_isolation_split[6].str())};
+    // if (std::smatch match_general_AB_numeric_isolation_split; std::regex_search(source_u, match_general_AB_numeric_isolation_split, re_flu_name_general_AB_numeric_isolation_split)) {
+    //     const std::array fields{match_general_AB_numeric_isolation_split[1].str(), match_general_AB_numeric_isolation_split[2].str(),
+    //                             fix_location(match_general_AB_numeric_isolation_split[3].str(), flags & parse_name_f::lookup_location),
+    //                             ::string::concat(match_general_AB_numeric_isolation_split[4].str(), '-', match_general_AB_numeric_isolation_split[5].str()),
+    //                             fix_year(match_general_AB_numeric_isolation_split[6].str())};
+    //     name = virus_name_t(::string::join("/", fields));
+    //     extra = make_extra(match_general_AB_numeric_isolation_split);
+    // }
+    // else if (std::smatch match_general_AB_location_split; std::regex_search(source_u, match_general_AB_location_split, re_flu_name_general_AB_location_split)) {
+    //     const std::array fields{match_general_AB_location_split[1].str(), match_general_AB_location_split[2].str(),
+    //                             fix_location(::string::concat(match_general_AB_location_split[3].str(), ' ', match_general_AB_location_split[4].str()), flags & parse_name_f::lookup_location),
+    //                             match_general_AB_location_split[5].str(),
+    //                             fix_year(match_general_AB_location_split[6].str())};
+    //     name = virus_name_t(::string::join("/", fields));
+    //     extra = make_extra(match_general_AB_location_split);
+    // }
+
+    if (std::smatch match_general_AB_isolation_with_location; std::regex_search(source_u, match_general_AB_isolation_with_location, re_flu_name_general_AB_isolation_with_location)) {
+        std::cerr << "??: " << source_u << ' ' << match_general_AB_isolation_with_location.format("[1: $1] [host: $2] [loc: $3] [iso1: $4] [iso2: $5] [y: $6]") << '\n';
+        auto location = fix_location(::string::concat(match_general_AB_isolation_with_location[3].str(), ' ', match_general_AB_isolation_with_location[4].str()), flags & parse_name_f::lookup_location, false);
+        auto isolation = match_general_AB_isolation_with_location[5].str();
+        if (!location.empty()) {
+            if (isolation[0] == '-' || isolation[0] == '_' || isolation[0] == ' ')
+                isolation.erase(0);
+        }
+        else {
+            location = fix_location(match_general_AB_isolation_with_location[3].str(), flags & parse_name_f::lookup_location);
+            isolation = ::string::concat(match_general_AB_isolation_with_location[4].str(), isolation);
+        }
+        const std::array fields{match_general_AB_isolation_with_location[1].str(), match_general_AB_isolation_with_location[2].str(),
+                                location, isolation, fix_year(match_general_AB_isolation_with_location[6].str())};
         name = virus_name_t(::string::join("/", fields));
-        extra = make_extra(match_general_AB_numeric_isolation_split);
+        extra = make_extra(match_general_AB_isolation_with_location);
     }
-    else if (std::smatch match_general_AB_location_split; std::regex_search(source_u, match_general_AB_location_split, re_flu_name_general_AB_location_split)) {
-        const std::array fields{match_general_AB_location_split[1].str(), match_general_AB_location_split[2].str(),
-                                fix_location(::string::concat(match_general_AB_location_split[3].str(), ' ', match_general_AB_location_split[4].str()), flags & parse_name_f::lookup_location),
-                                match_general_AB_location_split[5].str(),
-                                fix_year(match_general_AB_location_split[6].str())};
-        name = virus_name_t(::string::join("/", fields));
-        extra = make_extra(match_general_AB_location_split);
-    }
-    else if (std::smatch match_general_AB;
-        std::regex_search(source_u, match_general_AB, re_flu_name_general_AB) || std::regex_search(source_u, match_general_AB, re_flu_name_general_AB_no_slash_after_location)) {
-        const std::array fields{match_general_AB[1].str(), match_general_AB[2].str(), fix_location(match_general_AB[3].str(), flags & parse_name_f::lookup_location), match_general_AB[4].str(),
-                                fix_year(match_general_AB[5].str())};
+    else if (std::smatch match_general_AB; std::regex_search(source_u, match_general_AB, re_flu_name_general_AB)) {
+        // std::cerr << "??: " << source_u << ' ' << match_general_AB.format("[1: $1] [host: $2] [loc: $3] [iso: $4], [y: $5]") << '\n';
+        auto host = match_general_AB[2].str();
+        auto location = fix_location(match_general_AB[3].str(), flags & parse_name_f::lookup_location, false);
+        auto isolation = match_general_AB[4].str();
+        if (location.empty() && !host.empty()) {
+            location = fix_location(host, flags & parse_name_f::lookup_location, false);
+            if (location.empty())
+                throw LocationNotFound(match_general_AB[3].str());
+            host.clear();
+            isolation = ::string::join("-", {match_general_AB[3].str(), isolation});
+        }
+        const std::array fields{match_general_AB[1].str(), host, location, isolation, fix_year(match_general_AB[5].str())};
         name = virus_name_t(::string::join("/", fields));
         extra = make_extra(match_general_AB);
+    }
+    else if (std::smatch match_general_AB_no_isolation; std::regex_search(source_u, match_general_AB_no_isolation, re_flu_name_general_AB_no_isolation)) {
+        // std::cerr << "??: " << source_u << ' ' << match_general_AB_no_isolation.format("[1: $1] [host: $2] [loc: $3] [y: $4]") << '\n';
+        const std::array fields{match_general_AB_no_isolation[1].str(), match_general_AB_no_isolation[2].str(),
+                                fix_location(match_general_AB_no_isolation[3].str(), flags & parse_name_f::lookup_location), match_general_AB_no_isolation[4].str(),
+                                fix_year(match_general_AB_no_isolation[5].str())};
+        name = virus_name_t(::string::join("/", fields));
+        extra = make_extra(match_general_AB_no_isolation);
     }
     else if (std::smatch match_general_A_subtype; std::regex_search(source_u, match_general_A_subtype, re_flu_name_general_A_subtype)) {
         const std::array fields{match_general_A_subtype[1].str(), match_general_A_subtype[2].str(), fix_location(match_general_A_subtype[3].str(), flags & parse_name_f::lookup_location),
@@ -112,8 +163,9 @@ std::tuple<acmacs::virus::virus_name_t, acmacs::virus::Reassortant, acmacs::viru
         name = virus_name_t(::string::join("/", fields));
         extra = make_extra(match_general_A_subtype);
     }
-    else
+    else {
         throw Error("cannot parse: " + std::string(source));
+    }
 
     Reassortant reassortant;
     if (!extra.empty())
@@ -142,11 +194,21 @@ std::tuple<acmacs::virus::virus_name_t, acmacs::virus::Reassortant, acmacs::viru
 
 // ----------------------------------------------------------------------
 
-std::string fix_location(std::string source, acmacs::virus::parse_name_f flags)
+std::string fix_location(std::string source, acmacs::virus::parse_name_f flags, bool raise)
 {
     if (flags != acmacs::virus::parse_name_f::lookup_location)
         return source;
-    return get_locdb().find(::string::strip(source)).name;
+    try {
+        return get_locdb().find(::string::strip(source)).name;
+    }
+    catch (LocationNotFound& err) {
+        if (raise) {
+            std::cerr << "LocationNotFound: " << err.what() << '\n';
+            throw;
+        }
+        else
+            return {};
+    }
 
 } // fix_location
 
