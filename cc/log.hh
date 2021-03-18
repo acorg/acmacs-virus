d2a50f0fe5fb7e9f23a104e30454fd9aee5277bf
Eugene Skepner <github@skepner.eu>
2021-03-18 15:09:32 +0100

    acmacs::log support for acmacs_py

--- cc/log.hh
+++ cc/log.hh
@@ -4,11 +4,19 @@
 
 // ----------------------------------------------------------------------
 
+#pragma GCC diagnostic push
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wexit-time-destructors"
+#pragma GCC diagnostic ignored "-Wglobal-constructors"
+#endif
+
 namespace acmacs::log::inline v1
 {
-    constexpr log_key_t name_parsing{"name"};
-    constexpr log_key_t passage_parsing{"passage"};
+    const log_key_t name_parsing{"name"};
+    const log_key_t passage_parsing{"passage"};
 }
+
+#pragma GCC diagnostic pop
 
 // ----------------------------------------------------------------------
 /// Local Variables:
